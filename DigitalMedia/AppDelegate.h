5dcefc750073d18d1e472241e69d05dd6740df03
kingboyrang <aJia@bago.com.tw>
2014-05-15 16:14:08 +0800

    Initial Commit

---
+++ DigitalMedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DigitalMedia
+//
+//  Created by aJia on 2014/5/15.
+//  Copyright (c) 2014年 lz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DigitalMedia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DigitalMedia
+//
+//  Created by aJia on 2014/5/15.
+//  Copyright (c) 2014年 lz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
